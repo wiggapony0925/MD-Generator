865a3d24cf4efbff76f4ba19bcea78b20b6b810c
wiggapony0925 <ninjeff06@gmail.com>
2023-08-13 08:47:31 -0400

    First mvp of app;ication

--- main.py
+++ main.py
@@ -1,16 +1,63 @@
+from types import prepare_class
+import openai
+import requests
+from dotenv import load_dotenv
+
+#outside Libraries
+import glob
+from markdown import markdown
+
+load_dotenv()
+
+
+APIKEY = os.getenv("OPENAIKEY")
+
+# check to see if token is active: 
+if APIKEY is not None:
+    print(f'this ApiKey is active {APIKEY}')
+else:
+    print("APIKEY is not active")
+    exit()
+
+URL = "NONE"
+
+
+class MDGenerator:
+    def __init__(self, file, projectName):
+        self.file = file
+        self.markdown_content = " "
+        self.projectName = projectName
+
+    def readFileContent(self):
+        python_files = 'FILE PATH OR INPUT'
+        glob.glob(os.path.join(self.source_directory, "*.py"))
+        #LOOP THROUGH CODE
+        code = ''
+        for python_file in python_files:
+            with open(python_files, 'r') as file:
+                code += file.read() + '\n'
+                return code
+                
+
+class OpenAi: 
+    def summarize_code(self, code):
+        prompt = f"summarize this code in MarkDown code so i can use it for my Github Project: \n\n{code}"
+        response = openai.Completion.create(
+                engine="text-davinci-003",
+                prompt=prompt,
+                max_tokens=100
+                )
+        return
+        response.choices[0].text.strip()
+
+     # define source dir
+source_directory = input("path/to/your/project:  ")
+
+def createDirectory(response):
+
+
+
+
+
+
 # This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
